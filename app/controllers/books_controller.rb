9bd67dc44199ca1859a4842f021dcb0060aa3a4e
Your Name <ueharattyo920@gmail.com>
2025-01-31 18:40:21 +0900

    paramsの理解

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,15 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
     end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
 end
