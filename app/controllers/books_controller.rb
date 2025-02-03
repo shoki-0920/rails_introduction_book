ec87777f9d6c22622c5e06470ef6a0476197b16a
Your Name <ueharattyo920@gmail.com>
2025-02-03 18:21:48 +0900

    edit,destroy,method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,14 +15,32 @@ class BooksController < ApplicationController
     def create
         book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
     @book = Book.new(book_params)
-    if @book.save
-      redirect_to books_path
-    else
-      render :new
+      if @book.save
+        redirect_to books_path
+      else
+       render :new
+      end
     end
 
     def edit
         @book = Book.find(params[:id])
     end
-end
+
+    def update
+        @book = Book.find(params[:id])
+        book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+        if @book.update(book_params)
+          redirect_to books_path
+        else
+          render :edit
+        end
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path
+    end
+    
+
 end
