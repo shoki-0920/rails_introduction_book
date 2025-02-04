fc898b0f9d1f4759515bcaf19a2937e582cbebf8
Your Name <ueharattyo920@gmail.com>
2025-02-04 19:23:58 +0900

    02/04flash,partial,resources

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,13 @@
 class BooksController < ApplicationController
 
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
     def index
         @books = Book.all
     end
 
     def show
-        @book = Book.find(params[:id])
+       
     end
 
     def new
@@ -16,30 +18,41 @@ class BooksController < ApplicationController
         book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
     @book = Book.new(book_params)
       if @book.save
+        flash[:notice] = "家計簿にデータを1件登録しました"
         redirect_to books_path
       else
+       flash.now[:alert] = "登録に失敗しました"
        render :new
       end
     end
 
     def edit
-        @book = Book.find(params[:id])
+        
     end
 
     def update
-        @book = Book.find(params[:id])
+        
         book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
         if @book.update(book_params)
+          flash[:notice] = "データを一件更新しました"
           redirect_to books_path
         else
+          flash.now[:notice] = "更新に失敗しました"
           render :edit
         end
     end
 
     def destroy
-        @book = Book.find(params[:id])
+        
         @book.destroy
+        flash[:notice] = "削除しました"
         redirect_to books_path
+    end
+
+    private
+
+    def set_book
+      @book = find(params[:id])
     end
     
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "/books", to: "books#index"
-  post "/books" , to: "books#create"
-  get "/books/new" , to: "books#new" , as: "new_book"
-  get "/books/:id/edit" , to: "books#edit", as: "edit_book"
-  get "/books/:id", to: "books#show", as: "book"
-  patch "?books/:id", to: "books#update"
-  delete "?books/:id", to: "books#destroy"
+  # get "/books", to: "books#index"
+  # post "/books" , to: "books#create"
+  # get "/books/new" , to: "books#new" , as: "new_book"
+  # get "/books/:id/edit" , to: "books#edit", as: "edit_book"
+  # get "/books/:id", to: "books#show", as: "book"
+  # patch "/books/:id", to: "books#update"
+  # delete "/books/:id", to: "books#destroy"
+  resources :books
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
